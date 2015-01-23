abdf82ba6a5f1d03da39feca8a6df56d4871ce17
johnlok <john.lok.djinn@gmail.com>
2015-01-23 14:22:39 +0800

    the fortune cookie

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -1,1 +1,15 @@
-fortune_cookie.py
+#the fortune cookie program
+
+import random
+
+cookie = raw_input("Type \"open\" to reveal your fortune...")
+
+while cookie != "open":
+  cookie = raw_input("You failed to open the cookie. Try again")
+
+fortune = random.randint(1,5)
+fortunes = ["Easy and slow will make your day flow","Great challenges await you","Be aware of the woman in the red shirt","Lift Moar","Shiggy Diggy"]
+
+print(fortunes[fortune])
+
+raw_input("Tap enter to exit.")
