813812242a6c9b62a3bc5e5ee77424c1a19386a5
johnlok <john.lok.djinn@gmail.com>
2015-01-23 22:15:35 +0800

    fixed randint, probably wouldn't show the last fortune

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -7,7 +7,7 @@ cookie = raw_input("Type \"open\" to reveal your fortune...")
 while cookie != "open":
   cookie = raw_input("You failed to open the cookie. Try again")
 
-fortune = random.randint(1,5)
+fortune = random.randrange(0,5)
 fortunes = ["Easy and slow will make your day flow","Great challenges await you","Be aware of the woman in the red shirt","Lift Moar","Shiggy Diggy"]
 
 print(fortunes[fortune])
